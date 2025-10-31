3ef81badae99e0051589a765e27777e7a2071fee
Victor Matia <vmatir@outlook.com>
2025-10-31 18:07:42 +0000

    Add `AppIdHint` to manage `SDL_APP_ID` hint with callback and priority support
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Vmr.Sdl/Hints/AppIdHint.cs
@@ -0,0 +1,44 @@
+// -----------------------------------------------------------------------
+// <copyright file="AppIdHint.cs" company="Vmr.Sdl">
+// Copyright (c) Vmr.Sdl. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Vmr.Sdl.NativeImports;
+
+namespace Vmr.Sdl.Hints;
+
+/// <summary>A variable setting the app ID string.</summary>
+/// <remarks>
+/// <para>This string is used by desktop compositors to identify and group windows together, as well as match applications with associated desktop settings and icons.</para>
+/// <para>This will override <see cref="ApplicationMetadata.Identifier"/>, if set by the application.</para>
+/// <para>This hint should be set before SDL is initialized.</para>
+/// </remarks>
+public class AppIdHint : HintBase
+{
+    private const string Hint = "SDL_APP_ID";
+
+    /// <summary>Gets or sets the value of the hint.</summary>
+    public static string? Value
+    {
+        get => NativeSdl.GetHint(Hint);
+        set => NativeSdl.SetHint(Hint, value);
+    }
+
+    /// <summary>Sets the hint with the given priority.</summary>
+    /// <param name="priority">The priority of the hint.</param>
+    /// <param name="value">The value of the hint.</param>
+    public static void Set(HintPriority priority, bool value) => SetWithPriority(Hint, value ? "1" : "0", priority);
+
+    /// <summary>Adds a callback to the hint.</summary>
+    /// <param name="callback">The callback to add.</param>
+    /// <remarks>For safety reasons, only ONE (1) callback will be active at a time, the last one. Adding new callbacks while not removing the old one, will result in the automatic removal of the previous one.</remarks>
+    /// <seealso cref="RemoveCallback"/>
+    public static void AddCallback(HintUpdated callback) => AddCallback(Hint, callback);
+
+    /// <summary>Removes the callback from the hint.</summary>
+    /// <seealso cref="AddCallback"/>
+    public static void RemoveCallback() => RemoveCallback(Hint);
+}
