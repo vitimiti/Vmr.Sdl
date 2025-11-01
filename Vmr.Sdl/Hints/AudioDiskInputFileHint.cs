5389ea6b8179ca129430e0d58ce313749f834d81
Victor Matia <vmatir@outlook.com>
2025-11-01 12:12:56 +0000

    Add `AudioDiskInputFileHint` to manage `SDL_AUDIO_DISK_INPUT_FILE` hint with callback and priority support
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Vmr.Sdl/Hints/AudioDiskInputFileHint.cs
@@ -0,0 +1,45 @@
+// -----------------------------------------------------------------------
+// <copyright file="AudioDiskInputFileHint.cs" company="Vmr.Sdl">
+// Copyright (c) Vmr.Sdl. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Vmr.Sdl.NativeImports;
+using Vmr.Sdl.Subsystems;
+
+namespace Vmr.Sdl.Hints;
+
+/// <summary>Specify the input file when recording audio using the disk audio driver.</summary>
+/// <remarks>You can set this hint through the environment variable "SDL_AUDIO_DISK_INPUT_FILE".</remarks>
+public class AudioDiskInputFileHint : HintBase
+{
+    private const string Hint = "SDL_AUDIO_DISK_INPUT_FILE";
+
+    /// <summary>Gets or sets the value of the hint.</summary>
+    /// <remarks>
+    /// <para>This defaults to "sdlaudio-in.raw".</para>
+    /// <para>This hint should be set before an audio device is opened (see <see cref="AudioSubsystem"/>.)</para>
+    /// </remarks>
+    public static string? Value
+    {
+        get => NativeSdl.GetHint(Hint);
+        set => SetHintValue(Hint, value);
+    }
+
+    /// <summary>Sets the hint with the given priority.</summary>
+    /// <param name="priority">The priority of the hint.</param>
+    /// <param name="value">The value of the hint.</param>
+    public static void Set(HintPriority priority, bool value) => SetWithPriority(Hint, value ? "1" : "0", priority);
+
+    /// <summary>Adds a callback to the hint.</summary>
+    /// <param name="callback">The callback to add.</param>
+    /// <remarks>For safety reasons, only ONE (1) callback will be active at a time, the last one. Adding new callbacks while not removing the old one, will result in the automatic removal of the previous one.</remarks>
+    /// <seealso cref="RemoveCallback"/>
+    public static void AddCallback(HintUpdated callback) => AddCallback(Hint, callback);
+
+    /// <summary>Removes the callback from the hint.</summary>
+    /// <seealso cref="AddCallback"/>
+    public static void RemoveCallback() => RemoveCallback(Hint);
+}
