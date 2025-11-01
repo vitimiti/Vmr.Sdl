1b39f65a7c23071a0e9d1f2ee9d9783b2e4848cb
Victor Matia <vmatir@outlook.com>
2025-11-01 12:20:43 +0000

    Add `AudioDriverHint` to manage `SDL_AUDIO_DRIVER` hint with callback and priority support
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Vmr.Sdl/Hints/AudioDriverHint.cs
@@ -0,0 +1,44 @@
+// -----------------------------------------------------------------------
+// <copyright file="AudioDriverHint.cs" company="Vmr.Sdl">
+// Copyright (c) Vmr.Sdl. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Vmr.Sdl.NativeImports;
+
+namespace Vmr.Sdl.Hints;
+
+/// <summary>A variable setting the application name.</summary>
+/// <remarks>
+/// <para>By default, SDL will try all available audio backends in a reasonable order until it finds one that can work, but this hint allows the app or user to force a specific driver, such as "pipewire" if, say, you are on PulseAudio but want to try talking to the lower level instead.</para>
+/// <para>You can set this hint through the environment variable "SDL_AUDIO_DRIVER".</para>
+/// </remarks>
+public class AudioDriverHint : HintBase
+{
+    private const string Hint = "SDL_AUDIO_DRIVER";
+
+    /// <summary>Gets or sets the value of the hint.</summary>
+    /// <remarks>This hint should be set before SDL is initialized (see <see cref="Application"/>.)</remarks>
+    public static string? Value
+    {
+        get => NativeSdl.GetHint(Hint);
+        set => SetHintValue(Hint, value);
+    }
+
+    /// <summary>Sets the hint with the given priority.</summary>
+    /// <param name="priority">The priority of the hint.</param>
+    /// <param name="value">The value of the hint.</param>
+    public static void Set(HintPriority priority, bool value) => SetWithPriority(Hint, value ? "1" : "0", priority);
+
+    /// <summary>Adds a callback to the hint.</summary>
+    /// <param name="callback">The callback to add.</param>
+    /// <remarks>For safety reasons, only ONE (1) callback will be active at a time, the last one. Adding new callbacks while not removing the old one, will result in the automatic removal of the previous one.</remarks>
+    /// <seealso cref="RemoveCallback"/>
+    public static void AddCallback(HintUpdated callback) => AddCallback(Hint, callback);
+
+    /// <summary>Removes the callback from the hint.</summary>
+    /// <seealso cref="AddCallback"/>
+    public static void RemoveCallback() => RemoveCallback(Hint);
+}
