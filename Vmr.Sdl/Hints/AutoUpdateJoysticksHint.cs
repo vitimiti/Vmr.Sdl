d248047f550dbb9ca9046a5134d22f7f2b505d71
Victor Matia <vmatir@outlook.com>
2025-11-01 12:37:08 +0000

    Add `AutoUpdateJoysticksHint` to manage `SDL_AUTO_UPDATE_JOYSTICKS` hint with callback and priority support
    Signed-off-by: Victor Matia <vmatir@outlook.com>

---
+++ Vmr.Sdl/Hints/AutoUpdateJoysticksHint.cs
@@ -0,0 +1,50 @@
+// -----------------------------------------------------------------------
+// <copyright file="AutoUpdateJoysticksHint.cs" company="Vmr.Sdl">
+// Copyright (c) Vmr.Sdl. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Vmr.Sdl.NativeImports;
+
+namespace Vmr.Sdl.Hints;
+
+/// <summary>A variable controlling whether SDL updates the joystick state when getting input events.</summary>
+/// <remarks>You can set this hint through the environment variable "SDL_AUTO_UPDATE_JOYSTICKS" with the values "1" to enable or "0" to disable.</remarks>
+public class AutoUpdateJoysticksHint : HintBase
+{
+    private const string Hint = "SDL_AUTO_UPDATE_JOYSTICKS";
+
+    // TODO: Change SDL_UpdateJoysticks to our object docs
+
+    /// <summary>Gets or sets a value indicating whether the hint is enabled or not.</summary>
+    /// <remarks>
+    /// The variable can be set to the following values:
+    /// <list type="bullet">
+    /// <item><see langword="true"/> (Default): SDL will automatically call SDL_UpdateJoysticks().</item>
+    /// <item><see langword="false"/>: You'll call SDL_UpdateJoysticks() manually.</item>
+    /// </list>
+    /// This hint should be set before SDL is initialized (see <see cref="Application"/>.)
+    /// </remarks>
+    public static bool Value
+    {
+        get => NativeSdl.GetHintBoolean(Hint, defaultValue: true);
+        set => SetHintValue(Hint, value ? "1" : "0");
+    }
+
+    /// <summary>Sets the hint with the given priority.</summary>
+    /// <param name="priority">The priority of the hint.</param>
+    /// <param name="value">The value of the hint.</param>
+    public static void Set(HintPriority priority, bool value) => SetWithPriority(Hint, value ? "1" : "0", priority);
+
+    /// <summary>Adds a callback to the hint.</summary>
+    /// <param name="callback">The callback to add.</param>
+    /// <remarks>For safety reasons, only ONE (1) callback will be active at a time, the last one. Adding new callbacks while not removing the old one, will result in the automatic removal of the previous one.</remarks>
+    /// <seealso cref="RemoveCallback"/>
+    public static void AddCallback(HintUpdated callback) => AddCallback(Hint, callback);
+
+    /// <summary>Removes the callback from the hint.</summary>
+    /// <seealso cref="AddCallback"/>
+    public static void RemoveCallback() => RemoveCallback(Hint);
+}
